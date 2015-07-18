68d424deb21436cd1ea9ac50244aeedf7e828c81
Daniel Tabak <daniel@tabak.ca>
2015-07-18 14:26:00 -0400

    Removed useless routers

--- src/routers/main.jsx
+++ src/routers/main.jsx
@@ -4,7 +4,7 @@ import { Route, DefaultRoute, RouteHandler, Link } from "react-router";
 import HomePage from "../pages/home/page";
 
 
-export default class LoggedInRouter extends React.Component {
+export default class MainRouter extends React.Component {
   render() {
     return (
       <div id="container">
@@ -25,7 +25,7 @@ export default class LoggedInRouter extends React.Component {
 
   static getRoutes = function() {
     return (
-      <Route name="app" path="/" handler={LoggedInRouter}>
+      <Route name="app" path="/" handler={MainRouter}>
         <DefaultRoute name="home" handler={HomePage} />
       </Route>
     );
